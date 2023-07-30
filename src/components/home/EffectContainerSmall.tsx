f2a1d3766c30d2d3f77a08582ee6d0a16d1077ae
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-30 16:35:25 +0530

    Completed Footer

---
+++ src/components/home/EffectContainerSmall.tsx
@@ -0,0 +1,33 @@
+import Profile from '../../assets/images/kavishka.png';
+import { useRef } from 'react';
+import SlickSection from '../SmallComps/SlickSection';
+import { motion } from 'framer-motion';
+
+const EffectContainerSmall = () => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const styles = {
+    background: {
+      background: "radial-gradient(circle, rgba(0,212,255,0.2) 0%, rgba(9,9,121,0) 95%)"
+    },
+
+  }
+  return (
+    <motion.div className=" select-none relative w-64 rounded-xl border-2 border-cyan-500/10 backdrop-blur-sm mx-auto flex justify-center content-end transition-all duration-500 hover:transform hover:-translate-x-2 hover:translate-y-15"
+      style={styles.background}
+      animate={{ scale: 1, x: 0 }}
+      initial={{ scale: 0, x: 100 }}
+      transition={{ ease: "easeOut", duration: 0.5, type: "spring", bounce: 1 }}
+    >
+      <img src={Profile} className='w-28 object-cover rounded-xl' loading='lazy' />
+      <div className='flex flex-col justify-center text-center font-semibold bg-cyan-800 py-1 rounded-xl mx-auto my-4 absolute w-44 left-0 right-0 bottom-0 opacity-100' ref={divRef}>
+        <SlickSection list={["Full Stack Developer", "Undergraduate Student", "Video Editor", "Member of ISACA"]} />
+      </div>
+      {/* <div className='absolute w-56 h-56 bg-white rounded-full -bottom-20'>
+
+      </div> */}
+    </motion.div>
+  )
+}
+
+export default EffectContainerSmall
